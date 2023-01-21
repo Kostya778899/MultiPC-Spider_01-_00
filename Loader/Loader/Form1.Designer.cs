2e388f9349dedb2a9b8b296354baa0fd409fe41d
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2023-01-21 16:50:04 +0500

    .

--- Loader/Loader/Form1.Designer.cs
+++ Loader/Loader/Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Name = "Form1";
+            this.Text = "Loader";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Loader/Loader/Program.cs
+++ Loader/Loader/Program.cs
@@ -17,6 +17,8 @@ namespace Loader
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
